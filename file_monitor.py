f5723e4bb9c0b3db23effa92ed2e4ea6b4ea3362
Shmorgworks <aidankelley333@gmail.com>
2022-12-15 13:59:34 -0500

    new static files generated

--- file_monitor.py
+++ file_monitor.py
@@ -9,22 +9,20 @@ import os
 #folder to the github account, else, do nothing. The output will become the 
 #input to this program through sys.stdin
 
-while True:
-	if (sys.stdin):
+# fswatch -o path | xargs -n 1 path
 
-	     #copy the static files 
-	     os.system('cp -R /var/www/html/static/* ~/Documents/shmorgworks/shmorgworks')
-	     os.system('cd ~/Documents/shmorgworks/shmorgworks')
-	     #making sure we are deploying to the deployment branch
-	     os.system('git checkout deployment')
-	     #add and push all the changes. Git Cli should cache the login information for 
-	     #shmorgworks github. downloaded as "gh".
-	     os.system('git add -A')
-	     os.system('git commit -m "new static files generated"')
-	     os.system('git push')
+#copy the static files 
+os.system('cp -R /var/www/html/static/* ~/Documents/shmorgworks/shmorgworks')
+os.system('cd ~/Documents/shmorgworks/shmorgworks')
+#making sure we are deploying to the deployment branch
+os.system('git checkout deployment')
+#add and push all the changes. Git Cli should cache the login information for 
+#shmorgworks github. downloaded as "gh".
+os.system('git add -A')
+os.system('git commit -m "new static files generated"')
+os.system('git push')
 
-	     #verifying changes occured
-	     print('operation completed')
-	else:
-		continue
+#verifying changes occured
+print('operation completed')
+
 #os.system('your command')
